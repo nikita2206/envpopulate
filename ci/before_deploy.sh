7e45ed2d16757a3e0f5795d9665cb5d7a172cc5c
Nikita Nefedov <inefedor@gmail.com>
2019-01-28 14:28:08 +0300

    test

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -21,7 +21,9 @@ main() {
     cross rustc --bin envpopulate --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/envpopulate $stage/
+    #cp target/$TARGET/release/envpopulate $stage/
+
+    ls target
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *

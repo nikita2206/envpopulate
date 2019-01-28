99018887486ae001d1bd47de0a8407e2fe0284c0
Nikita Nefedov <inefedor@gmail.com>
2019-01-28 14:36:25 +0300

    dasd

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -23,7 +23,7 @@ main() {
     # TODO Update this to package the right artifacts
     #cp target/$TARGET/release/envpopulate $stage/
 
-    ls target
+    ls target/x86_64-pc-windows-gnu
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
